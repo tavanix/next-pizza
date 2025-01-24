0da90ac4810b3c8e605f89fe602dde105d967cfd
Maksim Aisin <maksim.aisin@mail.ru>
2025-01-24 22:09:42 +0300

    init project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <h1>
+      Home
+      <Button variant='outline'>Корзина</Button>
+    </h1>
+  )
+}
