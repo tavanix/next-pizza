0a642fc578ab64ea0497c8903eca3f0fba7182f3
Maksim Aisin <maksim.aisin@mail.ru>
2025-01-26 21:32:02 +0300

    feat: layout setup, ui components, header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
+import { Header } from '@/components/shared'
 
 const nunito = Nunito({
   subsets: ['cyrillic'],
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={nunito.className}>
-        <main className='min-h-screen'>{children}</main>
+        <main className='min-h-screen'>
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return (
-    <h1>
-      Home
-      <Button variant='outline'>Корзина</Button>
-    </h1>
-  )
+  return <h1>Home</h1>
 }
